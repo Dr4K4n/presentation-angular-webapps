ffc12ab01c4c92ca9bf4bf400c3cc982bcc55c6f
Dr4K4n <stefan@dr4k4n.de>
2014-10-09 00:35:07 +0200

    more pictures! first demo (doitlive), some content for the "HOW?" part, notes

---
+++ demos/cordova/session.sh
@@ -0,0 +1,19 @@
+# Recorded with the doitlive recorder
+#doitlive shell: /bin/bash
+#doitlive prompt: default
+
+cordova create hello com.prodyna.hello HelloWorld
+
+cd hello
+
+find .
+
+cordova platform add android
+
+find .
+
+cordova build
+
+cordova run --device
+
+
